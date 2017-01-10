23cbb40f6f5da158eb759610443a48de30466c06
Jian Wei <jianwei@jianwei-deb8-64>
2017-01-10 11:13:41 -0500

    second edit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <memory>
 #include <algorithm>
+#include <ctime>
+#include <cstdlib>
 
 struct ListNode {
     int val;
@@ -34,13 +36,48 @@ void printList(const pNode & head) {
 class solution {
 public:
     solution (const pNode & head)
-	: begin(head) {}
+	: begin(head), curr(head) {}
 
     int getRandom() {
-	
+        srand(time(NULL));
+        pNode next = curr->next;
+        if (next == nullptr)
+            next = begin;
+        while (next != nullptr) {
+            int range = distance(begin,next)+1;
+            int rnd = int(rand())%range;
+            // std::cout << "range = " << range << ", rnd = " << rnd << "; ";
+            if (rnd == 1)
+                curr = next;
+            next = next->next;
+            // std::cout << curr->val << ";";
+        }
+        return curr->val;
+    }
+
+  private:
+    int distance(const pNode & head, const pNode & np) {
+        int dist = 0;
+        pNode p = head;
+        while (p != nullptr && p != np) {
+            dist++;
+            p = p->next;
+        }
+        return dist;
     }
 
 private:
-    pNode curr;
+    pNode begin, curr;
 };
 
+int main() {
+    std::vector<int> nums{1,2,3,4,5};
+    pNode head = createList(nums);
+    printList(head);
+
+    solution soln(head);
+
+    for (size_t i = 0; i < nums.size(); i++)
+        std::cout << soln.getRandom() << " ";
+    std::cout << std::endl;
+}
